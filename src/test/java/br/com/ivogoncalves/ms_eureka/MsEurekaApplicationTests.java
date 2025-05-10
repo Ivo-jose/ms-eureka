528bafe94c5f72f78074ebea002a4945a670c9c7
Ivo Goncalves <ivojmgs19@gmail.com>
2025-05-10 13:17:02 -0300

    Project Created

---
+++ src/test/java/br/com/ivogoncalves/ms_eureka/MsEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ivogoncalves.ms_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
